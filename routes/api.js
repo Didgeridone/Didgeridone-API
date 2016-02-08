349b66b7ddf8a03a5804bbdef14de6710b7e1631
dlinch <dlinch33@gmail.com>
2016-02-08 13:35:39 -0700

    express properly connects to a local mongodb and presents the data

---
+++ routes/api.js
@@ -0,0 +1,35 @@
+var express = require('express')
+var router = express.Router();
+var mongoose = require('mongoose');
+
+
+
+var data = {
+  key1: 'Stuff',
+  key2: 'More stuff'
+}
+
+
+
+router.get('/', function(req, res){
+  mongoose.connect('mongodb://localhost/didgeridone')
+  var db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function(){
+    console.log('open')
+    var taskSchema = mongoose.Schema({
+      _id: Number,
+      data: String
+    })
+    var Task = mongoose.model('Task', taskSchema)
+    Task.find(function(err, tasks){
+      res.json(tasks)
+    })
+
+  })
+
+
+
+})
+
+module.exports = router;
