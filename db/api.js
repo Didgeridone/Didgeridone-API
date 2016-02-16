42ffb9c596cffa4c6de798d3395d7c1c340d9600
davidtadams <adams.t.david@gmail.com>
2016-02-15 23:50:19 -0700

    fixing whitespace

--- db/api.js
+++ db/api.js
@@ -16,43 +16,40 @@ module.exports = {
         { _id: userID },
         {
           $set: {
-          first_name: data.first_name,
-          last_name: data.last_name,
-          email: data.email
+            first_name: data.first_name,
+            last_name: data.last_name,
+            email: data.email
           }
         }
       )
     },
     deleteUser: function(db, userID) {
-      return db.collection('users').deleteOne( { "_id": userID })
+      return db.collection('users').deleteOne( { "_id": userID } )
     }
   },
   tasks: {
     createTask: function(db, userID, data) {
-      console.log(data)
      return db.collection('users').update(
-        {_id: ObjectID(userID)},
-        {$push: {tasks: data}}
+        { _id: ObjectID(userID) },
+        { $push: { tasks: data } }
       )
     },
     getTasks: function(db, userID, data) {
-      return db.collection('users').find( {"_id": ObjectID(userID) } ).toArray()
+      return db.collection('users').find( { "_id": ObjectID(userID) } ).toArray()
     },
     updateTask: function(db, userID, taskID, data) {
       var set = {}
-      set['tasks.'+taskID]= data;
+      set['tasks.' + taskID] = data;
       return db.collection('users').update(
-        {_id: ObjectID(userID)},
-        {$set: set}
+        { _id: ObjectID(userID) },
+        { $set: set }
       )
     },
     deleteTask: function(db, userID, data) {
-      console.log('data.name', data.name)
-
       return db.collection('users').updateOne(
-          {_id: ObjectID(userID)},
-          {$pull: {tasks: {name: data.name}}}
-        )
+        { _id: ObjectID(userID) },
+        { $pull: { tasks: { name: data.name } } }
+      )
     }
   }
 }
