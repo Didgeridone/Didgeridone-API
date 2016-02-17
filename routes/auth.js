ef11dba5ab94f9c642a2441ca8c80e4202caccb0
davidtadams <adams.t.david@gmail.com>
2016-02-17 15:30:56 -0700

    saving progress with google auth"

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,9 @@
-var api = require('../db/api')
 var express = require('express')
 var router = express.Router()
-var db = require('../db/dbconnect')
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var api = require('../db/api')
+var db = require('../db/dbconnect')
 
 var env = {
   clientID: process.env.CLIENT_ID,
@@ -13,27 +13,46 @@ var env = {
 
 passport.use(new GoogleStrategy(
   env,
-  function(token, tokenSecret, profile, done){
+  function(accessToken, refreshToken, profile, done) {
     var user = insertUser(profile)
-    api.users.createUser(db.get, user).then(function(results) {
-      user.tasks = results.ops[0].tasks
-      user._id = results.ops[0]._id
-      done(null, user)
-    }).catch(function(error) {
-      res.json({
-        "error": "Error creating user.",
-        "message": error
+    findUserByEmail(user.email).then(function(user) {
+      done(null, {
+        user: user,
+        accessToken: accessToken
       })
+    }).catch(function(err) {
+      if (err.userNotFound) {
+        api.users.createUser(db.get(), user)
+        .then(function(results) {
+          user.tasks = results.ops[0].tasks
+          user._id = results.ops[0]._id
+          done(null, {
+            user: user,
+            accessToken: accessToken
+          })
+        }).catch(function(error) {
+          res.json({
+            "error": "Error creating user.",
+            "message": error
+          })
+          done(err)
+        })
+      } else {
+        done(err)
+      }
     })
   }
 ))
 
 router.get('/google/callback', function(req, res, next) {
   passport.authenticate('google', function(err, user, info) {
+    console.log('this is user: ', user);
+    console.log('this is err: ', err);
+    console.log('this is info: ', info);
     if (err) {
       next(err);
     } else if (user) {
-      req.logIn(user, function(err) {
+      req.In(user, function(err) {
         if (err) {
           next(err);
         } else {
@@ -64,6 +83,19 @@ function insertUser(profile){
     email: profile._json.emails[0].value
   }
   return user
+}
+
+function findUserByEmail(email) {
+  return api.users.findUser(db.get(), email)
+  .then(function(results) {
+    if (results.length > 0) {
+      return results[0]
+    } else {
+      return Promise.reject( { userNotFound:true } )
+    }
+  }).catch(function(err) {
+    return Promise.reject(err)
+  })
 }
 
 module.exports = {
